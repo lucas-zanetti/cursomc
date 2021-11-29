562e9c38b2770834e5bbad63220bccfa7d806e2c
Lucas Zanetti <lucas.hzanetti@gmail.com>
2021-11-29 12:22:18 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/lucaszanetti/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.lucaszanetti.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lucaszanetti.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
